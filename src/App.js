a3a7c0f0262f7afe83ce2bcfb3c5a70d38e0df09
dontpanicgr <dontpanic.cg@gmail.com>
2018-06-19 15:11:47 +0300

    Final

--- src/App.js
+++ src/App.js
@@ -1,21 +1,21 @@
 import React, { Component } from 'react';
 import './App.css';
 import logo from './icon.svg';
-import { Tooltip, Button, Radio, Icon } from 'antd';
+import { Tooltip, Button, Icon } from 'antd';
 
 /* DB */
 var song1 = ["Your own personal jesus someone to hear your prayers", "🎹 Depeche Mode - Personal Jesus", "https://www.youtube.com/watch?v=u1xrNaTO1bI"];
 var song2 = ["When you were here before couldn't look you in the eye", "😭 Radiohead - Creep", "https://www.youtube.com/watch?v=XFkzRNyygfk"];
-var song3 = ["Today is gonna be the day that they're gonna throw it back to you", "🕶 Oasis", "https://www.youtube.com/watch?v=bx1Bh8ZvH84"];
+var song3 = ["Today is gonna be the day that they're gonna throw it back to you", "🕶 Oasis - Wonderwall", "https://www.youtube.com/watch?v=bx1Bh8ZvH84"];
 var song4 = ["London calling to the faraway towns", "🤘 The Clash - London calling", "https://www.youtube.com/watch?v=EfK-WX2pa8c"];
 var song5 = ["Hey jude don't make it bad", "🎸 The Beatles - Hey Jude", "https://www.youtube.com/watch?v=A_MjCqQoLLA"];
 var song6 = ["This is the end my only friend the end", "👖 The Doors - The End", "https://www.youtube.com/watch?v=JSUIQgEVDM4"];
 var song7 = ["Mama just killed a man put a gun against his head", "👨🏻 Queen - Bohemian Rhapsody", "https://www.youtube.com/watch?v=fJ9rUzIMcZQ"];
-var song7 = ["In this life in this life in this life", "🦁 Bob Marley - Coming in from the cold", "https://www.youtube.com/watch?v=g3Z4PX2JI_c"];
+var song8 = ["In this life in this life in this life", "🦁 Bob Marley - Coming in from the cold", "https://www.youtube.com/watch?v=g3Z4PX2JI_c"];
 
 /* GENERATE */
 function generate() {
-  var values = [song1,song3,song2,song5,song4,song7,song6],
+  var values = [song1,song3,song2,song5,song4,song7,song6, song8],
       valueToUse = values[Math.floor(Math.random() * values.length)];
   // do something with the selected value
   document.getElementById("song").innerHTML = valueToUse[0];
@@ -32,8 +32,15 @@ function copyToClipboard()  {
   document.execCommand('copy')
   textField.remove()
 }
-
 const text = <span>Click to copy!</span>;
+
+document.onkeyup=function(e){
+  var e = e || window.event; // for IE to cover IEs window object
+  if(e.altKey && e.which == 65) {
+       alert('Keyboard shortcut working!');
+       return false;
+  }
+}
 
 class App extends Component {
   render() {
@@ -48,12 +55,12 @@ class App extends Component {
           <Tooltip placement="top" title={text}>
             <div onClick={copyToClipboard} className="password" id="song">Hey must be a devil between us</div>
           </Tooltip>     
-          <div className="name" id="singer">⚡️Pixies - Hey</div>
+          <div className="name" id="singer">⚡️ Pixies - Hey</div>
           <div className="url"><a id="link" href="https://www.youtube.com/watch?v=MDACd-ShjHk" target="blank"><Icon type="play-circle-o" style={{ fontSize: 12, padding: 4 }} />Play song</a></div>
           <div className="action"><Button onClick={generate} type="primary" icon="retweet" size="large">Generate new password</Button></div>
         </div>
         <div className="footer">
-        created by <a href="https://github.com/dontpanicgr">@dontpanicgr</a>
+        Created by <a href="https://github.com/dontpanicgr">@dontpanicgr</a>
         </div>
       </div>
     );
